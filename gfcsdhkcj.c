9e45367e0a075e837def36a3091b85536f523ff4
lovf <308409003@qq.com>
2020-05-18 10:02:12 +0800

    链表中倒数第K个节点

---
+++ gfcsdhkcj.c
@@ -0,0 +1,47 @@
+问题 13:链表中倒数第k个节点
+输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+例如，一个链表有6个节点，从头节点开始，它们的值依次是1、2、3、4、5、6。这个链表的倒数第3个节点是值为4的节点。
+示例：
+给定一个链表 : 1->2->3->4->5, 和 k = 2.返回链表 4->5.
+
+
+		 方法1.利用快慢指针求解
+	 第一步 : 首先确定倒数第几个节点, 就能确定快指针走到了哪一个位置
+	   第二步 : 快慢指针依次往后移动
+
+struct ListNode* getKthFromEnd(struct ListNode* head, int k){
+	struct ListNode* slow, *fast;
+	slow = fast = head;
+	while (k--) {
+		if (fast) {
+			fast = fast->next;
+		}
+		else {
+			return NULL;
+		}
+	}
+
+	while (fast) {
+		fast = fast = fast->next;
+		slow = slow->next;
+	}
+
+	return slow;
+}
+
+方法2:常规法求解
+第一步 : 对整个链表进遍历, 得到链表的长度
+  第二步 : 再把节点走n - k个位置, 就得到倒数第k个节点
+struct ListNode* getKthFromEnd(struct ListNode* head, int k){
+	struct ListNode* cur = head;
+	int idx = 0;
+	while (cur) {
+		idx++;
+		cur = cur->next;
+	}
+	cur = head;
+	for (int i = 0; i < idx - k; i++) {
+		cur = cur->next;
+	}
+	return cur;
+}
